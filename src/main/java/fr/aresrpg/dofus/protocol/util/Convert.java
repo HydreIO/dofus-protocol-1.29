0ea91eaaae52992bd221756356281071133d9eb4
Sceat <mr.sceat@outlook.com>
2016-11-22 02:56:36 +0100

    meme moi jpensait pas que jtrouvait cte methode

--- src/main/java/fr/aresrpg/dofus/protocol/util/Convert.java
+++ src/main/java/fr/aresrpg/dofus/protocol/util/Convert.java
@@ -1,14 +1,18 @@
 package fr.aresrpg.dofus.protocol.util;
 
 public class Convert {
-	public static int toInt(String value , int def , int base){
-		if(value.isEmpty())
+	public static int toInt(String value, int def, int base) {
+		if (value.isEmpty())
 			return def;
 		else
-			return Integer.parseInt(value , base);
+			return Integer.parseInt(value, base);
 	}
 
-	public static int toInt(String value , int def){
-		return toInt(value , def , 10);
+	public static int toInt(String value, int def) {
+		return toInt(value, def, 10);
+	}
+
+	public static String fromInt(int val) {
+		return val == 0 ? "" : Integer.toHexString(val);
 	}
 }
