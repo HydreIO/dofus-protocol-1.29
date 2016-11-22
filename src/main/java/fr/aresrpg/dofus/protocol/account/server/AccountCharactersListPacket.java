936bfc49614f12804f25cc0e1fe0022ecabef6d5
Sceat <mr.sceat@outlook.com>
2016-11-22 02:59:01 +0100

    @DeltaEvo est vraiment un gros flémard

--- src/main/java/fr/aresrpg/dofus/protocol/account/server/AccountCharactersListPacket.java
+++ src/main/java/fr/aresrpg/dofus/protocol/account/server/AccountCharactersListPacket.java
@@ -1,16 +1,15 @@
 package fr.aresrpg.dofus.protocol.account.server;
 
-import fr.aresrpg.dofus.protocol.DofusStream;
-import fr.aresrpg.dofus.protocol.Packet;
-import fr.aresrpg.dofus.protocol.PacketHandler;
+import fr.aresrpg.dofus.protocol.*;
 import fr.aresrpg.dofus.protocol.util.Convert;
 import fr.aresrpg.dofus.protocol.util.StringUtils;
 import fr.aresrpg.dofus.structures.character.AvailableCharacter;
 
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.StringJoiner;
 
-public class AccountCharactersListPacket implements Packet{
+public class AccountCharactersListPacket implements Packet {
 	private int subscriptionTime;
 	private AvailableCharacter[] characters;
 
@@ -19,32 +18,57 @@ public class AccountCharactersListPacket implements Packet{
 		subscriptionTime = stream.readInt();
 		int nb = stream.readInt();
 		characters = new AvailableCharacter[nb];
-		for(int i = 0 ; i < nb ; i++){
-			String[] data = StringUtils.split(stream.read() , ";");
+		for (int i = 0; i < nb; i++) {
+			String[] data = StringUtils.split(stream.read(), ";");
 			int id = Integer.parseInt(data[0]);
 			String pseudo = data[1];
 			int level = Integer.parseInt(data[2]);
 			int gfxId = Integer.parseInt(data[3]);
-			int color1 = Integer.parseInt(data[4] , 16);
-			int color2 = Integer.parseInt(data[5] , 16);
-			int color3 = Integer.parseInt(data[6] , 16);
+			int color1 = Integer.parseInt(data[4], 16);
+			int color2 = Integer.parseInt(data[5], 16);
+			int color3 = Integer.parseInt(data[6], 16);
 			String[] accessoriesData = StringUtils.split(data[7], ",");
 			int[] accessories = new int[accessoriesData.length];
-			for(int a = 0 ; a < accessories.length ; a++)
-				accessories[a] = Convert.toInt(accessoriesData[a] , 0 , 16);
+			for (int a = 0; a < accessories.length; a++)
+				accessories[a] = Convert.toInt(accessoriesData[a], 0, 16);
 			boolean merchant = Integer.parseInt(data[8]) == 1;
 			int serverId = Integer.parseInt(data[9]);
-			boolean isDead = Convert.toInt(data[10] , 0) == 1;
-			int deathCount = Convert.toInt(data[11] , 0);
-			int lvlMax = Convert.toInt(data[12] , 0);
-			characters[i] = new AvailableCharacter(id , pseudo , level , gfxId , color1 , color2 ,
-					color3 , accessories , merchant , serverId , isDead , deathCount , lvlMax);
+			boolean isDead = Convert.toInt(data[10], 0) == 1;
+			int deathCount = Convert.toInt(data[11], 0);
+			int lvlMax = Convert.toInt(data[12], 0);
+			characters[i] = new AvailableCharacter(id, pseudo, level, gfxId, color1, color2,
+					color3, accessories, merchant, serverId, isDead, deathCount, lvlMax);
 		}
 	}
 
 	@Override
 	public void write(DofusStream stream) throws IOException {
-		//TODO
+		stream.writeInt(subscriptionTime);
+		stream.writeInt(getCharacters().length);
+		for (AvailableCharacter c : getCharacters()) {
+			StringJoiner sb = new StringJoiner(";");
+			sb.add(s(c.getId()))
+					.add(c.getPseudo())
+					.add(s(c.getLevel()))
+					.add(s(c.getGfxId()))
+					.add(s(c.getColor1()))
+					.add(s(c.getColor2()))
+					.add(s(c.getColor3()));
+			StringJoiner accs = new StringJoiner(",");
+			for (int i : c.getAccessories())
+				accs.add(Convert.fromInt(i));
+			sb.add(accs.toString())
+					.add(c.isMerchant() ? s(1) : s(0))
+					.add(s(c.getServerId()))
+					.add(c.isDead() ? s(1) : s(0))
+					.add(s(c.getDeathCount()))
+					.add(s(c.getLvlMax()));
+			stream.write(sb.toString());
+		}
+	}
+
+	private static String s(Object o) { // juste car la flemme de faire des String.valueOf dans le write
+		return o.toString();
 	}
 
 	@Override
@@ -72,9 +96,6 @@ public class AccountCharactersListPacket implements Packet{
 
 	@Override
 	public String toString() {
-		return "AccountCharactersListPacket{" +
-				"subscriptionTime=" + subscriptionTime +
-				", characters=" + Arrays.toString(characters) +
-				'}';
+		return "AccountCharactersListPacket(" + "subscriptionTime:" + subscriptionTime + "|characters:" + Arrays.toString(characters) + ")[" + getId() + "]";
 	}
 }
