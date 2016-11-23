bee7e494e49f8ae2bb5a88dd971593e898d51560
Sceat <mr.sceat@outlook.com>
2016-11-23 08:12:39 +0100

    fix yellow error

--- src/main/java/fr/aresrpg/dofus/protocol/util/SwfVariableExtractor.java
+++ src/main/java/fr/aresrpg/dofus/protocol/util/SwfVariableExtractor.java
@@ -1,22 +1,18 @@
 package fr.aresrpg.dofus.protocol.util;
 
-import flash.swf.Action;
-import flash.swf.ActionHandler;
-import flash.swf.TagDecoder;
-import flash.swf.TagHandler;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
+
+import flash.swf.*;
 import flash.swf.actions.ConstantPool;
 import flash.swf.actions.Push;
 import flash.swf.tags.DoAction;
 
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.*;
-
 public class SwfVariableExtractor extends TagHandler {
 
-	private Map<String , Object> variables = new HashMap<>();
-
+	private Map<String, Object> variables = new HashMap<>();
 
 	public Map<String, Object> getVariables() {
 		return variables;
@@ -30,10 +26,11 @@ public class SwfVariableExtractor extends TagHandler {
 	private class SwfActionVariableExtractor extends ActionHandler {
 		private Object[] stack = new Object[2];
 		private String[] pool;
+
 		@Override
 		public void push(Push action) {
-			stack[1] = stack[0]; //Bubble stack
-			if(pool != null && action.value instanceof Short)
+			stack[1] = stack[0]; // Bubble stack
+			if (pool != null && action.value instanceof Short)
 				stack[0] = (pool[(short) action.value]);
 			else
 				stack[0] = action.value;
@@ -46,7 +43,7 @@ public class SwfVariableExtractor extends TagHandler {
 
 		@Override
 		public void setVariable(Action action) {
-			variables.put(String.valueOf(stack[1]) , stack[0]);
+			variables.put(String.valueOf(stack[1]), stack[0]);
 		}
 
 	}
