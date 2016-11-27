794273db3e6805a8895d08db4d350adb29e5d825
Sceat <mr.sceat@outlook.com>
2016-11-27 13:37:31 +0100

    erf

--- src/main/java/fr/aresrpg/dofus/protocol/game/server/GamePositionStartPacket.java
+++ src/main/java/fr/aresrpg/dofus/protocol/game/server/GamePositionStartPacket.java
@@ -1,6 +1,7 @@
 package fr.aresrpg.dofus.protocol.game.server;
 
 import fr.aresrpg.dofus.protocol.*;
+import fr.aresrpg.dofus.protocol.util.Crypt;
 
 import java.io.IOException;
 
@@ -11,14 +12,22 @@ import java.io.IOException;
 public class GamePositionStartPacket implements Packet {
 
 	private int currentTeam;
-
-	// WIP
+	private int[] placesTeam0;
+	private int[] placesTeam1;
 
 	@Override
 	public void read(DofusStream stream) throws IOException {
 		String var4 = stream.read();
 		String var5 = stream.read();
 		this.currentTeam = stream.readInt();
+		this.placesTeam0 = new int[var4.length() / 2];
+		this.placesTeam1 = new int[var5.length() / 2];
+		int i = 0;
+		for (int var7 = 0; var7 < var4.length(); var7 += 2, i++)
+			this.placesTeam0[i] = (Crypt.indexOfHash(var4.charAt(var7)) << 6) + Crypt.indexOfHash(var4.charAt(var7 + 1));
+		i = 0;
+		for (int var9 = 0; var9 < var5.length(); var9 += 2, i++)
+			this.placesTeam1[i] = (Crypt.indexOfHash(var5.charAt(var9)) << 6) + Crypt.indexOfHash(var5.charAt(var9 + 1));
 	}
 
 	@Override
--- src/main/java/fr/aresrpg/dofus/protocol/util/Crypt.java
+++ src/main/java/fr/aresrpg/dofus/protocol/util/Crypt.java
@@ -12,13 +12,6 @@ public final class Crypt {
 
 	private static final char[] HEX_CHAR = "0123456789ABCDEF".toCharArray();
 
-	private static final int[] ZKARRAY = new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38,
-			39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63 };
-
-	public static int decode64(char val) {
-		return ZKARRAY[val];
-	}
-
 	private Crypt() {
 	}
 
@@ -99,6 +92,10 @@ public final class Crypt {
 
 	public static int decryptPort(String ePort) {
 		return decryptPort(ePort.toCharArray());
+	}
+
+	public static char encode64(int var) {
+		return HASH[var];
 	}
 
 	public static int indexOfHash(char ch) {
