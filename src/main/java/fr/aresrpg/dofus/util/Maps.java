3dc81ddde2a200c692650e243d8e0a73ed0fde01
Sceat <mr.sceat@outlook.com>
2016-12-09 05:38:48 +0100

    sans dec j'ai passé 4 grosse heure a réfléchir a ce systeme u_u
    Mon idée est que le pathfinder marche mal car tu calcul le X et Y avec les diagonale de dofus et pas avec un carré normal donc j'ai éssayé de calculer les X et Y de chaque cell ça marche presque mais y'en a ou j'ai pas réussi, tout les cell en rouge faut retourner le modulo de width-1 mais c'est chaud u_u http://i.imgur.com/mtDz3pU.png (a tt les coup c'est une connerie et ça change rien au pathfinding mais bon)

--- src/main/java/fr/aresrpg/dofus/util/Maps.java
+++ src/main/java/fr/aresrpg/dofus/util/Maps.java
@@ -7,17 +7,18 @@ import fr.aresrpg.dofus.structures.map.DofusMap;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
-import java.util.Map;
+import java.util.*;
 
 public class Maps {
-	private Maps() {}
-
-	public static InputStream downloadMap(String assetsServer , int id , String subId) throws IOException {
-		return new URL( assetsServer + Constants.MAP_PATH + id + '_' + subId + "X.swf").openStream();
+	private Maps() {
 	}
 
-	public static InputStream downloadMap(int id , String subId) throws IOException {
-		return downloadMap(Constants.DEFAULT_DATA_URL , id , subId);
+	public static InputStream downloadMap(String assetsServer, int id, String subId) throws IOException {
+		return new URL(assetsServer + Constants.MAP_PATH + id + '_' + subId + "X.swf").openStream();
+	}
+
+	public static InputStream downloadMap(int id, String subId) throws IOException {
+		return downloadMap(Constants.DEFAULT_DATA_URL, id, subId);
 	}
 
 	public static DofusMap loadMap(Map<String, Object> data, String decryptKey) {
@@ -25,22 +26,66 @@ public class Maps {
 		String key = Crypt.prepareKey(decryptKey);
 		cellData = Crypt.decipherData(cellData, key, Integer.parseInt(Character.toString(Crypt.checksum(key)), 16) * 2);
 		Cell[] cells = Compressor.uncompressMap(cellData);
-		return new DofusMap((int) data.get("id"), (int)data.get("width"),
-				(int)data.get("height"), (int)data.get("musicId"),
-				(int)data.get("capabilities"), (boolean)data.get("bOutdoor"),
-				(int)data.get("backgroundNum"), cells);
+		return new DofusMap((int) data.get("id"), (int) data.get("width"),
+				(int) data.get("height"), (int) data.get("musicId"),
+				(int) data.get("capabilities"), (boolean) data.get("bOutdoor"),
+				(int) data.get("backgroundNum"), cells);
 	}
 
-	public static int getLine(int id , int width) {
-		return (int) (id  / (width -0.5));
+	public static int getLine(int id, int width) {
+		return (int) (id / (width - 0.5));
 	}
 
-	public static int getColumn(int id , int width) {
-		return (int) ((id  % (width -0.5)) * 2);
+	public static int getColumn(int id, int width) {
+		return (int) ((id % (width - 0.5)) * 2);
 	}
 
-	public static int getId(int column, int line , int width) {
-		return (int) (line * (width - 0.5) + column/2.0);
+	public static int getId(int column, int line, int width) {
+		return (int) (line * (width - 0.5) + column / 2.0);
 	}
 
+	public static void main(String[] args) {
+		System.out.println("line = " + line(293, 15, 17));
+		System.out.println(293 % 14);
+	}
+
+	static int line(int id, int width, int height) {
+		if (id == 0) return id;
+		int index = 0;
+		int max = width + height - 2;
+		int sub = width - 1;
+		int val = width * 2 - 1;
+		if (id <= val) return id % sub == 0 ? sub : id % sub;
+		while (id / sub >= max) {
+			id -= width;
+			index++;
+		}
+		while (id % sub != 0) {
+			id -= width;
+			index++;
+		}
+		return width - 1 + index;
+	}
+
+	public static void mazin(String[] args) {
+		int largeur = 15;
+		int hauteur = 17;
+		int max = largeur + hauteur - 1; // 31
+		int firstin = -1;
+		int second = -1;
+		int same = hauteur - largeur + 1; // hauteur tjr positive donc ntm
+
+		List<String> ss = new ArrayList<>();
+		for (int x = 0; x < max; x++) {
+			for (int y = 0; y < max; y++) {
+				if (y <= largeur - firstin && y >= largeur - second) {
+					ss.add(x + " " + y);
+				}
+			}
+			firstin--;
+			second++;
+		}
+		ss.forEach(System.out::println);
+		System.out.println("Length = " + ss.size());
+	}
 }
