764945a51958de96d98205c91ddaac23f64ceb0a
Duarte David <deltaduartedavid@gmail.com>
2016-12-11 22:41:59 +0100

    Add Lang assets

--- src/main/java/fr/aresrpg/dofus/Constants.java
+++ src/main/java/fr/aresrpg/dofus/Constants.java
@@ -3,4 +3,6 @@ package fr.aresrpg.dofus;
 public class Constants {
 	public static final String DEFAULT_DATA_URL = "http://staticns.ankama.com/dofus/gamedata/dofus/";
 	public static final String MAP_PATH = "maps/";
+	public static final String LANG_VERSION_PATH = "lang/versions_{lang}.txt";
+	public static final String LANG_PATH = "lang/swf/{type}_{lang}_{version}.swf";
 }
---
+++ src/main/java/fr/aresrpg/dofus/util/Lang.java
@@ -0,0 +1,63 @@
+package fr.aresrpg.dofus.util;
+
+import fr.aresrpg.dofus.Constants;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Lang {
+	private Lang(){}
+
+	public static Map<String , Integer> getLangVersion(String assetsServer, String lang) throws IOException {
+		String[] data =  getText(new URL(assetsServer + Constants.LANG_VERSION_PATH.replace("{lang}" , lang))).substring(3)/*Remove &f=*/.split("\\|");
+		Map<String , Integer> m = new HashMap<>();
+		for(String d : data){
+			String[] ld = d.split(",");
+			System.out.println(d);
+			m.put(ld[0] , Integer.parseInt(ld[2]));
+		}
+		return m;
+	}
+
+	public static Map<String , Integer> getLangVersion(String lang) throws IOException {
+		return getLangVersion(Constants.DEFAULT_DATA_URL , lang);
+	}
+
+	public static InputStream getLangData(String assetsServer, String lang , int version , String type) throws IOException {
+		return new URL(assetsServer + Constants.LANG_PATH
+				.replace("{lang}" , lang)
+				.replace("{version}", Integer.toString(version))
+				.replace("{type}" , type)
+		).openStream();
+	}
+
+	public static InputStream getLangData(String lang , int version , String type) throws IOException {
+		return getLangData(Constants.DEFAULT_DATA_URL , lang , version , type);
+	}
+
+
+	public static void main(String[] args) throws IOException {
+		Map<String , Integer> versions = getLangVersion("fr");
+		Map<String, Object> d = SwfVariableExtractor.extractVariable(getLangData("fr" , versions.get("items") , "items"));
+		System.out.println(d);
+	}
+
+	private static String getText(URL url) throws IOException {
+		BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream()));
+
+		StringBuilder response = new StringBuilder();
+		String inputLine;
+
+		while ((inputLine = in.readLine()) != null)
+			response.append(inputLine);
+
+		in.close();
+
+		return response.toString();
+	}
+}
