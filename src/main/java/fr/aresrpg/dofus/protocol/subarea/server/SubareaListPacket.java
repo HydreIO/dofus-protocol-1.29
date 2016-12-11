ea88a71b35dd6ef39efad53a80362031b8725931
Sceat <mr.sceat@outlook.com>
2016-12-11 07:09:55 +0100

    david code avec le cul

--- src/main/java/fr/aresrpg/dofus/protocol/subarea/server/SubareaListPacket.java
+++ src/main/java/fr/aresrpg/dofus/protocol/subarea/server/SubareaListPacket.java
@@ -1,32 +1,30 @@
 package fr.aresrpg.dofus.protocol.subarea.server;
 
-import fr.aresrpg.dofus.protocol.DofusStream;
-import fr.aresrpg.dofus.protocol.Packet;
-import fr.aresrpg.dofus.protocol.PacketHandler;
+import fr.aresrpg.dofus.protocol.*;
 import fr.aresrpg.dofus.structures.Alignment;
 import fr.aresrpg.dofus.structures.map.Subarea;
+import fr.aresrpg.dofus.util.Convert;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class SubareaListPacket implements Packet{
+public class SubareaListPacket implements Packet {
 	private List<Subarea> subareas;
 
 	@Override
 	public void read(DofusStream stream) {
 		subareas = new ArrayList<>();
-		while (stream.available() > 0){
+		while (stream.available() > 0) {
 			String[] data = stream.read().split(";");
-			subareas.add(new Subarea(Integer.parseInt(data[0]) ,
-					Alignment.valueOf(data[1])));
+			subareas.add(new Subarea(Convert.toInt(data[0]), Alignment.valueOf(data.length > 1 ? Convert.toInt(data[1]) : 0)));
 		}
 	}
 
 	@Override
 	public void write(DofusStream stream) {
 		stream.allocate(subareas.size());
-		for(Subarea s : subareas)
-			stream.write(s.getId() + ";" + s.getAlignment().ordinal());
+		for (Subarea s : subareas)
+			stream.write(Convert.toDofusString(s.getId()) + ";" + s.getAlignment().ordinal());
 	}
 
 	@Override
