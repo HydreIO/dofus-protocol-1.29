1666a037e26246c412520ac9d22321a04e05e694
Sceat <mr.sceat@outlook.com>
2016-12-16 10:47:10 +0100

    MARQUE DE FRABRIQUE BABY

--- src/main/java/fr/aresrpg/dofus/protocol/game/movement/MovementPlayer.java
+++ src/main/java/fr/aresrpg/dofus/protocol/game/movement/MovementPlayer.java
@@ -1,4 +1,13 @@
 /*******************************************************************************
+ * BotFather (C) - Dofus 1.29 protocol library
+ * This class is part of an AresRPG Project.
+ *
+ * @author Sceat {@literal <sceat@aresrpg.fr>}
+ * @author Duarte David {@literal <deltaduartedavid@gmail.com>}
+ *  
+ * Created 2016
+ *******************************************************************************/
+/*******************************************************************************
  * BotFather (C) - Dofus 1.29
  * This class is part of an AresRPG Project.
  *
--- src/main/java/fr/aresrpg/dofus/protocol/item/server/ItemRemovePacket.java
+++ src/main/java/fr/aresrpg/dofus/protocol/item/server/ItemRemovePacket.java
@@ -1,3 +1,12 @@
+/*******************************************************************************
+ * BotFather (C) - Dofus 1.29 protocol library
+ * This class is part of an AresRPG Project.
+ *
+ * @author Sceat {@literal <sceat@aresrpg.fr>}
+ * @author Duarte David {@literal <deltaduartedavid@gmail.com>}
+ *  
+ * Created 2016
+ *******************************************************************************/
 package fr.aresrpg.dofus.protocol.item.server;
 
 import fr.aresrpg.dofus.protocol.*;
