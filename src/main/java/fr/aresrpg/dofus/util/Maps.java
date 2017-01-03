fb8aa03f3a1678cc7552b61fa710265a26729e6d
Duarte David <deltaduartedavid@gmail.com>
2017-01-03 17:50:00 +0100

    Fix rotated ids

--- src/main/java/fr/aresrpg/dofus/util/Maps.java
+++ src/main/java/fr/aresrpg/dofus/util/Maps.java
@@ -87,10 +87,24 @@ public class Maps {
 	private static final double COS_mPId4 = COS_PId4;
 	private static final double SIN_mPId4 = -SIN_PId4;
 
-	public static int getIdRotated(int x, int y, int width, int height) {
-		int id = (width + height - 1 - x) * width;
-		return y > height ? id - (y - height) * (width - 1) : y < height ? id + (height - y) * (width - 1) : id;
+	public static int getXRotated(int id, int width, int height) {
+		int x = getX(id , width) - width;
+		int y = getY(id , width) - height;
+		return (int)Math.ceil((x*COS_PId4 - y*SIN_PId4 - 0.25) * 0.7) + width;
 	}
+	public static int getYRotated(int id, int width, int height) {
+		int x = getX(id , width) - width;
+		int y = getY(id , width) - height;
+		return (int)Math.ceil((x*SIN_PId4 + y*COS_PId4 - 1.75) * 0.7) + height;
+	}
+	public static int getIdRotated(int xRot, int yRot, int width, int height) {
+		double xR = Math.ceil(xRot - width)/0.7 + 0.25;
+		double xY = Math.ceil(yRot - height)/0.7 + 1.75;
+		int x = (int)Math.round(xR*COS_mPId4 - xY*SIN_mPId4 - 0.1) + width;
+		int y = (int)Math.round(xR*SIN_mPId4 + xY*COS_mPId4) + height;
+		return getId(x, y, width);
+	}
+
 
 	public static void main(String[] args) {
 		int id = 50;
@@ -105,7 +119,7 @@ public class Maps {
 		System.out.println("final id = " + idSceat);
 	}
 
-	public static int getXRotated(int id, int width, int height) {
+	public static int getXRotatedSceat(int id, int width, int height) {
 		int n84 = width * (width * 2 - 2);
 		if (id <= n84 && id % width == 0) return getXOfMultiple(id, width, height);
 		int var = id;
@@ -121,7 +135,7 @@ public class Maps {
 		return xOfMultiple;
 	}
 
-	public static int getYRotated(int id, int width, int height) {
+	public static int getYRotatedSceat(int id, int width, int height) {
 		int n84 = width * (width * 2 - 2);
 		if (id <= n84 && id % width == 0) return height;
 		int var = id;
@@ -135,6 +149,11 @@ public class Maps {
 		while (count-- > 0)
 			yOfMultiple--;
 		return yOfMultiple;
+	}
+
+	public static int getIdRotatedSceat(int x, int y, int width, int height) {
+		int id = (width + height - 1 - x) * width;
+		return y > height ? id - (y - height) * (width - 1) : y < height ? id + (height - y) * (width - 1) : id;
 	}
 
 	private static int getXupper(int id, int width, int height) {
