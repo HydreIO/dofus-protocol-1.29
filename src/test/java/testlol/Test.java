029e99f40d5085ce498dfbe6c63d5ab98a6a613b
Duarte David <deltaduartedavid@gmail.com>
2017-01-03 17:24:46 +0100

    Merge branch 'develop' of /home/david/workspace/java/dofus-protocol-1.29 with conflicts.

--- src/test/java/testlol/Test.java
+++ src/test/java/testlol/Test.java
@@ -1,10 +1,9 @@
 package testlol;
 
+import fr.aresrpg.dofus.structures.map.DofusMap;
 import fr.aresrpg.dofus.util.DofusMapView;
 import fr.aresrpg.dofus.util.Maps;
-
-import java.awt.Point;
-
+import fr.aresrpg.dofus.util.SwfVariableExtractor;
 import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
@@ -12,6 +11,8 @@ import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+
+import java.awt.*;
 
 public class Test extends Application {
 
@@ -24,23 +25,17 @@ public class Test extends Application {
 	public void start(Stage stage) throws Exception {
 		DofusMapView v = new DofusMapView();
 		stage.setTitle("Hello World");
-		/*
-		 * stage.setScene(new Scene(v));
-		 * //[RCV:]<< GameMapDataPacket(mapId=1140, subid='0710011719', decryptKey='
-		 * 203b3c4e4e25324244564a4f755a7b574965445b6e2532426429423c70552d493a5f26674d24235a5e6f2c204e4844634a70296642557552753a712e41282036612f586d7f68753e5f3379325d56752777284f6e545547715b627d666c263f5f465441445929247021536c7f5e606a4270412532426f4560237d6f49622d34695047346b42586550436f6a7e7563727d5b7f227f736c7c6451293
-		 * ')[GDM]
-		 * DofusMap map = Maps.loadMap(
-		 * SwfVariableExtractor.extractVariable(Maps.downloadMap(1140, "0710011719")),
-		 * "203b3c4e4e25324244564a4f755a7b574965445b6e2532426429423c70552d493a5f26674d24235a5e6f2c204e4844634a70296642557552753a712e41282036612f586d7f68753e5f3379325d56752777284f6e545547715b627d666c263f5f465441445929247021536c7f5e606a4270412532426f4560237d6f49622d34695047346b42586550436f6a7e7563727d5b7f227f736c7c6451293b"
-		 * );
-		 * v.setMap(map);
-		 * stage.setResizable(true);
-		 * stage.show();
-		 * System.out.println(map.getHeight());
-		 * System.out.println(map.getWidth() + " " + map.getCells().length);
-		 */
-
-		Cell[] cells = new Cell[cellsW];
+		/* stage.setScene(new Scene(v));
+		 //GDM|7554|0905131019|50504d4065244a7a6c345c75562c5a3068215e395c487e75612532422d3330543730787e402a4a3776774657233a3e79303f3b3074742e227121657831584a253242406f786e6e226b5d6569236f30605c652252452f465524253235674e3a7d2e6c683a722933796b396c3c74332671347c2067753b40547a775c3d6f233e533241285c4050717e77482d4b7a3628362e433141682d655a694d283b4d6d4544532e7c305c76483767725e48
+		DofusMap map = Maps.loadMap(
+		 SwfVariableExtractor.extractVariable(Maps.downloadMap(7554 , "0905131019")),
+		 "50504d4065244a7a6c345c75562c5a3068215e395c487e75612532422d3330543730787e402a4a3776774657233a3e79303f3b3074742e227121657831584a253242406f786e6e226b5d6569236f30605c652252452f465524253235674e3a7d2e6c683a722933796b396c3c74332671347c2067753b40547a775c3d6f233e533241285c4050717e77482d4b7a3628362e433141682d655a694d283b4d6d4544532e7c305c76483767725e48"
+		 );
+		 v.setMap(map);
+		 stage.setResizable(true);
+		 stage.show();
+		 System.out.println("Map " + map.getHeight() + " " + map.getWidth() + " " + map.getCells().length);
+		*/Cell[] cells = new Cell[cellsW];
 		for (int i = 0; i < cells.length; i++)
 			cells[i] = new Cell(false, false);
 		cells[50] = new Cell(true, false);
@@ -48,10 +43,10 @@ public class Test extends Application {
 		cells[69] = new Cell(false, true);
 		Canvas canvas1 = new Canvas();
 		Canvas canvas2 = new Canvas();
-		canvas1.setWidth(1000);
-		canvas2.setWidth(1000);
-		canvas1.setHeight(1000);
-		canvas2.setHeight(1000);
+		canvas1.setWidth(1200);
+		canvas2.setWidth(1200);
+		canvas1.setHeight(1200);
+		canvas2.setHeight(1200);
 		drawCell(canvas1.getGraphicsContext2D(), canvas2.getGraphicsContext2D(), cells);
 		//drawRay(canvas1.getGraphicsContext2D());
 		Group r = new Group();
@@ -59,7 +54,6 @@ public class Test extends Application {
 		r.getChildren().add(canvas2);
 		stage.setScene(new Scene(r));
 		stage.show();
-
 	}
 
 	private void drawCell(GraphicsContext gc, GraphicsContext gid, Cell[] cells) {
@@ -71,30 +65,26 @@ public class Test extends Application {
 				gc.setFill(Color.AQUA);
 			else
 				gc.setFill(Color.LIGHTGRAY);
-			Point p = new Point(Maps.getXSceat(i, width, height), Maps.getYSceat(i, width, height));
-			int xe = Maps.getXSceat(i, width, height);
-			System.out.println("x " + p.x + " " + xe);
-			int ye = Maps.getYSceat(i, width, height);
-			System.out.println("y " + p.y + " " + ye);
-			double xp = p.x * 30;
-			double yp = p.y * 30;
+			int xe = Maps.getXRotated(i , width , height);
+			int ye = Maps.getYRotated(i , width , height);
+
+			System.out.println(xe + " " + ye);
+			System.out.println(i + " " + Maps.getIdRotated(xe , ye , width , height));
+			double xp = xe * 30;
+			double yp = ye * 30;
 			gc.fillRect(xp, yp, 29, 29);
 			/*
 			 * gc.fillPolygon(new double[] { xp, xp + 20, xp, xp - 20 },
 			 * new double[] { yp + 20, yp, yp - 20, yp }, 4);
 			 */
 			//gid.fillText(i + "", xp, yp + gc.getFont().getSize() / 3);
-			//gid.fillText(p.x + " " + p.y, xp + 9, yp + 16);
-			gid.setFill(Color.RED);
-			int idSceat = Maps.getIdSceat(p.x, p.y, width, height);
-			//if (idSceat == 120) gid.setFill(Color.AQUAMARINE);
-			gid.fillText(Maps.getIdSceat(p.x, p.y, width, height) + "", xp + 11, yp + 15);
-			gid.setFill(Color.BLACK);
+			gid.fillText(i + "", xp, yp + 16);
+
 		}
 	}
 
 	private void drawRay(GraphicsContext gc) {
-		Point p = /* Maps.getXY(50 , width , height); */null;
+		Point p = /*Maps.getXY(50 , width , height);*/null;
 		int y = p.y;
 		int x = p.x;
 		int r = range * 4 * 2;
@@ -107,7 +97,7 @@ public class Test extends Application {
 			System.out.println(ay);
 			for (int e = 1; e <= range; e++) {
 				System.out.println(i + " " + y + " " + (y + ay * e));
-				int id = Maps.getIdSceat((int) (x + ax * e), (int) Math.rint(y + ay * e), width, height);
+				int id = Maps.getIdRotated((int) (x + ax * e), (int) Math.rint(y + ay * e), width, height);
 				t(gc, id);
 			}
 			gc.setStroke(Color.BLUEVIOLET);
@@ -118,8 +108,8 @@ public class Test extends Application {
 	}
 
 	private void t(GraphicsContext gc, int id) {
-		int y = Maps.getYSceat(id, width, height);
-		float x = Maps.getXSceat(id, width, height);
+		int y = Maps.getYRotated(id, width, height);
+		float x = Maps.getXRotated(id, width, height);
 		double xp = x * 30 + 32;
 		double yp = y * 30 + 32;
 		gc.fillRect(xp, yp, 25, 25);
